bfb793117574d2280a306c0f98a9537cade0dade
Maxime Mondini <mh-mondini@hotmail.com>
2025-07-29 18:50:54 +0200

    setup redux toolkits

--- vite-redux-toolkit/src/main.jsx
+++ vite-redux-toolkit/src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import { Provider as ReduxProvider } from 'react-redux';
+import store from './store/store';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
   </React.StrictMode>
 );
