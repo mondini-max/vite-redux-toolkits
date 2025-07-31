16e0a4f9aa7328c6280bc566921be884dbe6b151
Maxime Mondini <mh-mondini@hotmail.com>
2025-07-31 13:39:00 +0200

    Adds cart functionality
    Adds the cart container and cart item components to display cart information.
    This includes displaying the cart items, total amount, and a clear cart button.

--- vite-redux-toolkit/src/App.jsx
+++ vite-redux-toolkit/src/App.jsx
@@ -1,11 +1,13 @@
 import { Fragment } from 'react';
 import Navbar from './component/Navbar.jsx';
+import { CartContainer } from './component/CartContainer.jsx';
 
 function App() {
   return (
     <Fragment>
       <main>
         <Navbar />
+        <CartContainer />
       </main>
     </Fragment>
   );
---
+++ vite-redux-toolkit/src/component/CartContainer.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import CartItem from './CartItem.jsx';
+import { useSelector } from 'react-redux';
+
+export const CartContainer = () => {
+  const { cartItems, amount, total } = useSelector((store) => store.cart);
+  return amount <= 0 ? (
+    <>
+      <section className='cart'>
+        <header>
+          <h2>Your bag</h2>
+          <h4 className='empty-cart'>is currently empty</h4>
+        </header>
+      </section>
+    </>
+  ) : (
+    <>
+      <section className='cart'>
+        <header>
+          <h2>Your Bag</h2>
+        </header>
+        <div>
+          {cartItems.map((item) => {
+            return <CartItem key={item.id} {...item} />;
+          })}
+        </div>
+        <footer>
+          <hr />
+          <div className='cart-total'>
+            <h4>
+              total<span>${total}</span>
+            </h4>
+          </div>
+          <button className='btn clear-btn'>clear cart</button>
+        </footer>
+      </section>
+    </>
+  );
+};
