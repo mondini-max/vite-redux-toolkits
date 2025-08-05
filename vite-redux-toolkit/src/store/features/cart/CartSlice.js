25cb1ae960d90061ff3bc22499989167a39ec22b
Maxime Mondini <mh-mondini@hotmail.com>
2025-08-05 11:52:07 +0200

    Updates cart amount calculation
    Improves cart functionality by centralizing the cart amount calculation into a reusable function.
    
    This ensures consistency and simplifies the logic for updating the amount in the cart.
    Also exports getAmount action to calculate the amount when needed.

--- vite-redux-toolkit/src/store/features/cart/CartSlice.js
+++ vite-redux-toolkit/src/store/features/cart/CartSlice.js
@@ -3,9 +3,13 @@ import cartItems from '../../../cartItems.js';
 
 const initialState = {
   cartItems: cartItems,
-  amount: 1,
+  amount: 0,
   total: 0,
   isLoading: true,
+};
+
+const calcTotalAmount = (cartItems) => {
+  return cartItems.reduce((sum, item) => sum + item.amount, 0);
 };
 
 const cartSlice = createSlice({
@@ -28,6 +32,8 @@ const cartSlice = createSlice({
         // console.log(item.id, itemId);
         return item.id !== itemId;
       });
+
+      state.amount = calcTotalAmount(state.cartItems);
     },
 
     increaseAmount: (state, { payload }) => {
@@ -36,18 +42,20 @@ const cartSlice = createSlice({
       console.log(JSON.stringify(cartItem));
       if (cartItem) {
         cartItem.amount += 1;
-        state.amount = state.cartItems.reduce(
-          (sum, item) => sum + item.amount,
-          0
-        );
+        state.amount = calcTotalAmount(state.cartItems);
       }
       console.log(JSON.stringify(state));
+    },
+
+    getAmount: (state) => {
+      state.amount = calcTotalAmount(state.cartItems);
     },
   },
 });
 
 console.log({ cartSlice });
 // console.log(cartSlice.reducer);
-export const { clearCart, removeItem, increaseAmount } = cartSlice.actions;
+export const { clearCart, removeItem, increaseAmount, getAmount } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
